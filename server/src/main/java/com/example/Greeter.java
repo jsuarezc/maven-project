3988eb33864c42e624f6210bde830c090fd87645
excalibur <job.suarez.c@gmail.com>
2018-04-11 13:52:28 +0200

    two commit from eclipse

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,11 @@ public class Greeter {
 
   }
 
+  /**
+   * 
+   * @param someone the name
+   * @return Greeting String
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
